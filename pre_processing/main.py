56c72eb7234c7583d1b6845f2ee0fe72d7c344b2
bjbomfim <abjbomfim@outlook.com>
2023-10-30 14:50:50 -0300

    Colocando CLAHE

--- pre_processing/main.py
+++ pre_processing/main.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 import pre_processing as pp
-import cv2
+import cv2 as cv
 import argparse
 import os
 
@@ -11,8 +11,8 @@ def pre_processing(images):
 def save_images(images, path: str):
     print("Saving images")
     for image in images:
-        save_file = f"{path}/{image.name}.jpg"
-        cv2.imwrite(save_file, image.image)
+        save_file = f"{path}/{image.name}"
+        cv.imwrite(save_file, image.image)
     print("Images saved completely")
 
 def load_images(images_paths, path):
@@ -20,7 +20,7 @@ def load_images(images_paths, path):
     ImageTuple = namedtuple("Image", ["name", "image"])
     for image_name in images_paths:
         image_path = os.path.join(path, image_name)
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
         if image is not None:
             images.append(ImageTuple(image_name, image))
     return images
@@ -42,21 +42,25 @@ def main():
     
     parser = argparse.ArgumentParser(description="Calculate medians of a vector.")
     parser.add_argument("-path", required=True, type=str)
-    parser.add_argument("-pathToSave", required=True, type=str)
+    parser.add_argument("-pathToSaveFirst", required=True, type=str)
+    parser.add_argument("-pathToSaveSecond", required=True, type=str)
     
     args = parser.parse_args()
     
     path = args.path
-    path_to_save = args.pathToSave
+    path_to_save_equalized = args.pathToSaveFirst
+    path_to_save_CLAHE = args.pathToSaveSecond
     
-    print(f"Args received: path: {path} pathToSave: {path_to_save}")
+    print(f"Args received: path: {path} pathToSave1: {path_to_save_equalized} pathToSave2: {path_to_save_CLAHE}")
     
-    images_names = read_images(path, path_to_save)
+    images_names = read_images(path, path_to_save_equalized)
     
-    for group in range(100, len(images_names), 100):
+    for group in range(100, 1000, 100):
         print(f"Group: {group}")
         images = load_images(images_names[group-100:group], path)
-        processed_images = pre_processing(images)
-        save_images(processed_images, path_to_save)
+        processed_images_equalized = pre_processing(images, 1)
+        save_images(processed_images_equalized, path_to_save_equalized)
+        processed_images_CLAHE = pre_processing(images, 2)
+        save_images(processed_images_CLAHE, path_to_save_CLAHE)
 
 main()
--- pre_processing/pre_processing.py
+++ pre_processing/pre_processing.py
@@ -1,19 +1,20 @@
 from collections import namedtuple
-import cv2
-import numpy as np
+import cv2 as cv
 
-
-def equalize_histogram(image):
-
-    equalized_histogram = cv2.equalizeHist(image)
+def equalize_histogram(image, tecnic):
+    equalized_histogram
+    if tecnic == 1 :
+        equalized_histogram = cv.equalizeHist(image)
+    else:
+        equalized_histogram = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     return equalized_histogram
 
-def equalize_histogram_images(images):
+def equalize_histogram_images(images, tecnic):
     
     ImageTuple = namedtuple("Image", ["name", "image"])
     print("Equalize Histogram")
     equalized_images = []
     for image in images:
-        equalized_images.append(ImageTuple(image.name, equalize_histogram(image.image)))
+        equalized_images.append(ImageTuple(image.name, equalize_histogram(image.image, tecnic)))
         
     return equalized_images
