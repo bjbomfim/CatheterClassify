2f16975889f3d4ab669dbe2c0719a9da828fdb93
bjbomfim <abjbomfim@outlook.com>
2023-11-17 14:59:05 -0300

    removendo arquivo ja existente

--- pre_processing/rasterization/main.py
+++ pre_processing/rasterization/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import argparse
 import cv2
 from enum import Enum
+import os
 
 
 class Labels(Enum):
@@ -33,7 +34,11 @@ def maskCreation(points, path, height, width, image_exist):
             x2, y2 = points[x_y+1]
             cv2.line(image, (x1, y1), (x2, y2), (235, 75, 52), 8)
         cv2.circle(image, (x1, y1), radius=9, color=(235, 75, 52), thickness=-1)
-    
+    # Verificar se o arquivo existe
+    if os.path.exists(path):
+        # Excluir o arquivo existente
+        os.remove(path)
+        print("Arquivo existente removido com sucesso!")
     cv2.imwrite(path, image)
 
 def rasterization(points: list, path_to_save: str, size_image: str):
@@ -43,7 +48,7 @@ def rasterization(points: list, path_to_save: str, size_image: str):
     for item in points:
         mask_save_path = path_to_save+'/'+item[0]+'.jpg'
         image = size_image.loc[size_image['StudyInstanceUID']==item[0]]
-        # print(image)
+        print(image)
         if image is not None:
             height = image['Height'].values[0]
             width = image['Width'].values[0]
@@ -53,7 +58,7 @@ def rasterization(points: list, path_to_save: str, size_image: str):
             else:
                 maskCreation(item[1], mask_save_path, height, width, True)
                 cache[item[0]] += 1
-                print(item[0])
+                print(f"{item[0]} ------ {cache[item[0]]}")
     
 
 def main():
@@ -91,3 +96,5 @@ def main():
     rasterization(points, path_to_save_rasterizarion, reader_csv_size_images)
 
 main()
+
+
