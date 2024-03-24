92cd5a7976e1d7c954981078e1087ee05e945032
bjbomfim <abjbomfim@outlook.com>
2024-03-24 12:48:30 -0300

    Ultimas mudanças

---
+++ main.py
@@ -0,0 +1,43 @@
+
+import os
+from dotenv import load_dotenv
+import csv
+
+from . import train
+
+def segmentation_train(train_ids, val_ids):
+    train.train(train_ids=train_ids, val_ids=val_ids)
+    
+def main(model_name_train = "segmentacao"):
+    
+    load_dotenv()
+    # TREINO DE SEGMENTAÇÃO
+    # Caminhos para os dados de treino
+    train_csv_path = os.getenv("TRAIN_CSV_PATH")
+    val_csv_path = os.getenv("TEST_CSV_PATH")
+    
+    train_ids = []
+    val_ids = []
+
+    with open(train_csv_path,'r') as folder_csv:
+        read_csv = csv.reader(folder_csv)
+        
+        for line in read_csv:
+            train_ids.append(line)
+
+        # Removendo titulo colunas
+        train_ids.pop(0)
+
+    with open(val_csv_path,'r') as folder_csv:
+        read_csv = csv.reader(folder_csv)
+        
+        for line in read_csv:
+            val_ids.append(line)
+        
+        # Removendo titulo colunas
+        val_ids.pop(0)
+    
+    if model_name_train == "segmentacao":
+        segmentation_train(train_ids, val_ids)
+    
+main()
---
+++ models/CustomCallbacks/save_data_train_results.py
@@ -0,0 +1,13 @@
+from os import system
+class SaveDataTrainResults(keras.callbacks.Callback):
+    
+    def __init__ (self, path_to_get_resultus, path_to_save):
+        super().__init__()
+        self.path_to_get_resultus = path_to_get_resultus
+        self.path_to_save = path_to_save
+        
+    
+    def on_epoch_begin(self, epoch, logs=None):
+        if epoch % 5 == 0:
+            print("Salvando resultados do treino")
+            system(f"cp -r {self.path_to_get_results} {self.path_to_save}")
