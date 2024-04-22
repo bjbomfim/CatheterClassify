d17d2ae3c1a28235bf0e08abdbab2ba4724a0d9b
bjbomfim <abjbomfim@outlook.com>
2024-04-22 10:49:50 -0300

    removendo o predictresults

--- models/binary_classify.py
+++ models/binary_classify.py
@@ -139,7 +139,7 @@ def predict_tube():
     print("Lendo as mascaras")
     images_list = os.listdir(path)
     # Popular o map de imagens
-    images_map = {key:0 for key in images_list if key != "semtubo.jpg" or key != "predictresults.csv"}
+    images_map = {key:0 for key in images_list if key != "predictresults.csv"}
     print(f"Total de mascaras: {len(images_map)}")
     # Fazer a predicao
     for key, value in images_map.items():
