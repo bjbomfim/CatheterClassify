7dd6c76ff0a1626cc25c104faa33d781a425f47d
bjbomfim <abjbomfim@outlook.com>
2024-05-09 16:40:53 -0300

    mudando o ocngelamento

--- models/CustomCallbacks/layer_trainable.py
+++ models/CustomCallbacks/layer_trainable.py
@@ -10,12 +10,6 @@ class layerTrainable(Callback):
     
     
     def on_train_begin(self, logs=None):
-        for i in range(5):
-            layer_index = self.layer_lenght - i - 1
-            self.zerar_pesos(self.model.layers[layer_index])
-            print("Zerando a camada: ", self.model.layers[layer_index].name)
-        
-    def zerar_pesos(self, layer):
-        pesos = layer.get_weights()
-        pesos_zerados = [tf.zeros_like(w) for w in pesos]
-        layer.set_weights(pesos_zerados)
+        for i in range(10):
+            self.model.layers[i].trainable = False
+            print("Congelado a camada mais profunda: ", self.model.layers[i].name)
