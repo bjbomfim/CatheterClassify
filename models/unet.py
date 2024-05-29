22c5c6b945c357f3684c939a65a859c8be990c1c
bjbomfim <abjbomfim@outlook.com>
2024-05-29 14:04:31 -0300

    teste

--- models/unet.py
+++ models/unet.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, concatenate, MaxPooling2D, UpSampling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications import ResNet50
+from tensorflow.keras import layers
 
 def unet_decoder(encoder_inputs, skip_connections):
     # Extract the number of filters from skip connections
@@ -27,24 +28,39 @@ def build_custom_unet(image_shape, mask_shape):
     # ResNet50 Encoder
     base_model = ResNet50(include_top=False, weights='imagenet', input_tensor=input1)
     
-    # Extract skip connections
-    skip_connections = [
-        base_model.get_layer('conv1_relu').output,
-        base_model.get_layer('conv2_block3_out').output,
-        base_model.get_layer('conv3_block4_out').output,
-        base_model.get_layer('conv4_block6_out').output,
-    ]
+    # # Extract skip connections
+    # skip_connections = [
+    #     base_model.get_layer('conv1_relu').output,
+    #     base_model.get_layer('conv2_block3_out').output,
+    #     base_model.get_layer('conv3_block4_out').output,
+    #     base_model.get_layer('conv4_block6_out').output,
+    # ]
     
-    # Bottom layer
-    encoder_output = base_model.get_layer('conv5_block3_out').output
+    # # Bottom layer
+    # encoder_output = base_model.get_layer('conv5_block3_out').output
     
-    # Build the UNet decoder
-    decoder_output = unet_decoder(encoder_output, skip_connections)
+    # # Build the UNet decoder
+    # decoder_output = unet_decoder(encoder_output, skip_connections)
     
     # Optional: Add a final conv layer to get desired output channels
-    output = Conv2D(1, (1, 1), activation='sigmoid')(decoder_output)
+    
+    x_image = base_model.output
+    x_image = layers.GlobalAveragePooling2D()(x_image)
+    
+    mask_input = Input(shape=mask_shape, name='mask_input')
+    x_mask = Conv2D(64, (3, 3), activation='relu', padding='same')(mask_input)
+    x_mask = layers.GlobalAveragePooling2D()(x_mask)
+    
+    x = layers.Concatenate()([x_image, x_mask])
+    x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    
+    output = Conv2D(1, (1, 1), activation='sigmoid')(x)
     
     # Create the model
     model = Model(inputs=[input1, input2], outputs=output)
+    
+    for layer in base_model.layers:
+        layer.trainable = True
     
     return model
