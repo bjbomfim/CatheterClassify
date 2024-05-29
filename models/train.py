a721eb17e64004991715877489e6a869aece3b34
bjbomfim <abjbomfim@outlook.com>
2024-05-29 13:56:24 -0300

    teste

--- models/train.py
+++ models/train.py
@@ -13,21 +13,6 @@ from .unet import build_custom_unet
 
 import tensorflow as tf
 
-def dice_loss(y_true, y_pred, smooth=1):
-    print(y_true)
-    print(y_pred)
-    y_true_f = tf.keras.backend.flatten(y_true)
-    y_pred_f = tf.keras.backend.flatten(y_pred)
-    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
-    return 1 - (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + smooth)
-
-def iou(y_true, y_pred, smooth=1):
-    y_true_f = tf.keras.backend.flatten(y_true)
-    y_pred_f = tf.keras.backend.flatten(y_pred)
-    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
-    union = tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) - intersection
-    return (intersection + smooth) / (union + smooth)
-
 def train(train_df, val_df, return_train_path = None, multi_input = True):
     
     sm.set_framework('tf.keras')
