0938a99a60d46dbc6a03b7cf1fdc710dfb4707bd
bjbomfim <abjbomfim@outlook.com>
2024-05-31 16:50:19 -0300

    alterando o tipo de augmentation

--- models/data_generator.py
+++ models/data_generator.py
@@ -17,17 +17,20 @@ class DataGenerator(Sequence):
         if self.augment:
             self.augmenter = A.Compose([
                 A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.RandomRotate90(p=0.5),
-                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.5),
+                A.GridDistortion(num_steps=5, distort_limit=0.03, p=0.5),
+                A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5),
                 A.OneOf([
                     A.CLAHE(clip_limit=2),
                     A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2),
+                    A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20)
                 ], p=0.5),
                 A.OneOf([
-                    A.GaussNoise(),
-                    A.GaussianBlur(),
+                    A.GaussNoise(var_limit=(10.0, 50.0)),
+                    A.GaussianBlur(blur_limit=3),
+                    A.MotionBlur(blur_limit=3),
                 ], p=0.5),
+                A.CoarseDropout(max_holes=8, max_height=8, max_width=8, p=0.5),
             ])
     
     def __len__(self):
@@ -209,13 +212,32 @@ class DataGeneratorClassify(Sequence):
         return X, y
 
 class DataGeneratorClassifyTwoInputs(Sequence):
-    def __init__(self, dataframe, batch_size=4, image_size=(384, 384), shuffle=True):
+    def __init__(self, dataframe, batch_size=4, image_size=(384, 384), shuffle=False, augment=False):
         self.dataframe = dataframe
         self.batch_size = batch_size
         self.image_size = image_size
         self.shuffle = shuffle
         self.indexes = np.arange(len(self.dataframe))
         self.on_epoch_end()
+        self.augment = augment
+        if self.augment:
+            self.augmenter = A.Compose([
+                A.HorizontalFlip(p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.5),
+                A.GridDistortion(num_steps=5, distort_limit=0.03, p=0.5),
+                A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5),
+                A.OneOf([
+                    A.CLAHE(clip_limit=2),
+                    A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2),
+                    A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20)
+                ], p=0.5),
+                A.OneOf([
+                    A.GaussNoise(var_limit=(10.0, 50.0)),
+                    A.GaussianBlur(blur_limit=3),
+                    A.MotionBlur(blur_limit=3),
+                ], p=0.5),
+                A.CoarseDropout(max_holes=8, max_height=8, max_width=8, p=0.5),
+            ])
 
     def __len__(self):
         return int(np.ceil(len(self.dataframe) / self.batch_size))
@@ -240,14 +262,20 @@ class DataGeneratorClassifyTwoInputs(Sequence):
             # Carregar imagem de raio-X
             img = cv2.imread(img_path)
             img = cv2.resize(img, (self.image_size[1], self.image_size[0]))
-            img = img / 255.0
-
             # Carregar máscara de segmentação
             mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
             mask = cv2.resize(mask, (self.image_size[1], self.image_size[0]))
+
+            
+            if self.augment:
+                augmented = self.augmenter(image=img, mask=mask)
+                img = augmented['image']
+                mask = augmented['mask']
+
             mask = mask.astype(np.float32) / 255.0
             mask = np.expand_dims(mask, axis=-1)  # Adiciona uma dimensão para máscaras
-
+            img = img / 255.0
+            
             X_images.append(img)
             X_masks.append(mask)
             y.append(labels.astype(np.float32))
