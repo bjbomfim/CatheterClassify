a01c6669d8b9077efe19c2fc38c9d570ec23566b
bjbomfim <abjbomfim@outlook.com>
2024-06-06 19:47:19 -0300

    fim da avaliacao

--- models/data_generator.py
+++ models/data_generator.py
@@ -85,44 +85,35 @@ class DataGenerator(Sequence):
             else:
                 print(f"Erro ao carregar a imagem: {idx[0]}")
 
-        return np.array(I), np.array(X), np.array(Y)
+        return np.array(X), np.array(Y)
 
 class DataGeneratorRefinamento(Sequence):
-    def __init__(self, dataframe, batch_size=4, image_size=(384, 384), shuffle=False, augment = False):
+    def __init__(self, dataframe, batch_size=4, image_size=(384, 384), augment = False):
         self.dataframe = dataframe
         self.batch_size = batch_size
         self.image_size = image_size
-        self.shuffle = shuffle
         self.indexes = np.arange(len(self.dataframe))
         self.augment = augment
         if self.augment:
             self.augmenter = A.Compose([
                 A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.RandomRotate90(p=0.5),
-                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.5),
+                A.GridDistortion(num_steps=5, distort_limit=0.03, p=0.5),
+                A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.25),
                 A.OneOf([
                     A.CLAHE(clip_limit=2),
                     A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2),
+                    A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20)
                 ], p=0.5),
                 A.OneOf([
-                    A.GaussNoise(),
-                    A.GaussianBlur(),
+                    A.GaussNoise(var_limit=(10.0, 50.0)),
+                    A.GaussianBlur(blur_limit=(3, 5)),
                 ], p=0.5),
+                A.CoarseDropout(max_holes=8, max_height=8, max_width=8, p=0.5),
             ])
     
     def __len__(self):
         return int(np.ceil(len(self.dataframe) / self.batch_size))
-
-    def on_epoch_end(self):
-        if self.shuffle:
-            np.random.shuffle(self.indexes)
-
-    def blend_images(self, image, mask, alpha=0.5):
-        # Ensure mask has the same number of channels as image
-        mask_colored = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        blended = cv2.addWeighted(image, alpha, mask_colored, 1 - alpha, 0)
-        return blended
 
     def resize_image(self, img):
         img = cv2.resize(img, self.image_size)
@@ -136,7 +127,8 @@ class DataGeneratorRefinamento(Sequence):
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
         batch_data = [self.dataframe.iloc[k] for k in indexes]
 
-        X = []
+        X_img = []
+        X_mask = []
         Y = []
         I = []
         
@@ -153,10 +145,8 @@ class DataGeneratorRefinamento(Sequence):
                 mask = self.resize_image(mask)
                 predict = self.resize_image(predict)
                 
-                # Aqui deve ser feito a mesclagem do predit e do img
-                blended_img = self.blend_images(img, predict)
                 if self.augment:
-                    augmented = self.augmenter(image=blended_img, mask=mask)
+                    augmented = self.augmenter(image=img, mask=mask)
                     img = augmented['image']
                     mask = augmented['mask']
                     
@@ -164,12 +154,13 @@ class DataGeneratorRefinamento(Sequence):
                 mask = self.normalize_image(mask)
 
                 I.append(data['ID'])
-                X.append(img)
+                X_img.append(img)
+                X_mask.append(predict)
                 Y.append(mask)
             else:
                 print(f"Erro ao carregar a imagem: {data['ID']}")
 
-        return np.array(X), np.array(Y)
+        return [np.array(X_img), np.array(X_mask)], np.array(Y)
 
 
 class DataGeneratorClassify(Sequence):
@@ -217,13 +208,31 @@ class DataGeneratorClassify(Sequence):
         return X, y
 
 class DataGeneratorClassifyTwoInputs(Sequence):
-    def __init__(self, dataframe, batch_size=4, image_size=(384, 384), shuffle=True):
+    def __init__(self, dataframe, batch_size=4, image_size=(384, 384), shuffle=False, augment=False):
         self.dataframe = dataframe
         self.batch_size = batch_size
         self.image_size = image_size
         self.shuffle = shuffle
         self.indexes = np.arange(len(self.dataframe))
         self.on_epoch_end()
+        self.augment = augment
+        if self.augment:
+            self.augmenter = A.Compose([
+                A.HorizontalFlip(p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.5),
+                A.GridDistortion(num_steps=5, distort_limit=0.03, p=0.5),
+                A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.25),
+                A.OneOf([
+                    A.CLAHE(clip_limit=2),
+                    A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2),
+                    A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20)
+                ], p=0.5),
+                A.OneOf([
+                    A.GaussNoise(var_limit=(10.0, 50.0)),
+                    A.GaussianBlur(blur_limit=(3, 5)),
+                ], p=0.5),
+                A.CoarseDropout(max_holes=8, max_height=8, max_width=8, p=0.5),
+            ])
 
     def __len__(self):
         return int(np.ceil(len(self.dataframe) / self.batch_size))
@@ -248,13 +257,19 @@ class DataGeneratorClassifyTwoInputs(Sequence):
             # Carregar imagem de raio-X
             img = cv2.imread(img_path)
             img = cv2.resize(img, (self.image_size[1], self.image_size[0]))
-            img = img / 255.0
 
             # Carregar máscara de segmentação
             mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
             mask = cv2.resize(mask, (self.image_size[1], self.image_size[0]))
+            
+            if self.augment:
+                augmented = self.augmenter(image=img, mask=mask)
+                img = augmented['image']
+                mask = augmented['mask']
+            
             mask = mask.astype(np.float32) / 255.0
-            mask = np.expand_dims(mask, axis=-1)  # Adiciona uma dimensão para máscaras
+            img = img / 255.0
+            mask = np.expand_dims(mask, axis=-1)
 
             X_images.append(img)
             X_masks.append(mask)
