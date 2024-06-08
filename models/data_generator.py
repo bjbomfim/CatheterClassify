2652bf235f49c2996d6d11309348daaa1077324c
bjbomfim <abjbomfim@outlook.com>
2024-06-08 17:02:14 -0300

    avaliacao segmentation

--- models/data_generator.py
+++ models/data_generator.py
@@ -85,7 +85,7 @@ class DataGenerator(Sequence):
             else:
                 print(f"Erro ao carregar a imagem: {idx[0]}")
 
-        return np.array(X), np.array(Y)
+        return  np.array(I), np.array(X), np.array(Y)
 
 class DataGeneratorRefinamento(Sequence):
     def __init__(self, dataframe, batch_size=4, image_size=(384, 384), augment = False):
