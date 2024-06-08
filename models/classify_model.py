1520f818e2e2bfe60d9518bfbcfb1666ef9ef486
bjbomfim <abjbomfim@outlook.com>
2024-06-08 19:50:05 -0300

    Treino Classify

--- models/classify_model.py
+++ models/classify_model.py
@@ -23,30 +23,29 @@ def build_classification_model(input_shape):
 
     return model
 
-def build_classification_model2(image_shape, mask_shape):
-
-    image_input = Input(shape=image_shape, name='image_input')
+def build_classification_model2(image_shape):
+    # Modificar a forma da imagem para ter 6 canais
+    image_input = Input(shape=(image_shape[0], image_shape[1], 6), name='image_input')
+    
+    # Criar uma camada convolucional para reduzir os 6 canais para 3 canais
+    x = Conv2D(3, (1, 1), padding='same', name='conv_initial')(image_input)
+    
+    # Usar a arquitetura ResNet50, mas com a nova camada de entrada
     base_model_image = tf.keras.applications.ResNet50(
         weights='imagenet',
         include_top=False,
-        input_tensor=image_input
+        input_tensor=x
     )
 
     x_image = base_model_image.output
     x_image = layers.GlobalAveragePooling2D()(x_image)
 
-    # Entrada para a máscara de segmentação
-    mask_input = Input(shape=mask_shape, name='mask_input')
-    x_mask = Conv2D(64, (3, 3), activation='relu', padding='same')(mask_input)
-    x_mask = layers.GlobalAveragePooling2D()(x_mask)
-
-    # Concatenar
-    x = layers.Concatenate()([x_image, x_mask])
-    x = layers.Dense(1024, activation='relu')(x)
+    # Adicionar camadas densas e dropout para a classificação final
+    x = layers.Dense(1024, activation='relu')(x_image)
     x = layers.Dropout(0.5)(x)
     predictions = layers.Dense(3, activation='sigmoid')(x)
 
-    model = models.Model(inputs=[image_input, mask_input], outputs=predictions)
+    model = models.Model(inputs=image_input, outputs=predictions)
 
     for layer in base_model_image.layers:
         layer.trainable = True
