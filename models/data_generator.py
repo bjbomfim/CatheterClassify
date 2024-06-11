9a3b2dfd0c0c976b101d65d848cd26b7af87d970
bjbomfim <abjbomfim@outlook.com>
2024-06-11 14:07:42 -0300

    bugfix

--- models/data_generator.py
+++ models/data_generator.py
@@ -177,6 +177,7 @@ class DataGeneratorClassifyTwoInputs(Sequence):
 
             # Carregar máscara de segmentação
             mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
             mask = cv2.resize(mask, (self.image_size[1], self.image_size[0]))
             
             if self.augment:
@@ -186,7 +187,6 @@ class DataGeneratorClassifyTwoInputs(Sequence):
             
             mask = mask.astype(np.float32) / 255.0
             img = img / 255.0
-            mask = np.expand_dims(mask, axis=-1)
 
             combined_data = np.concatenate([img, mask], axis=-1)
             
